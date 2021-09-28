48ac6d8eb9c134ac3f9730717d19565695941424
Bryan <squeld@outlook.fr>
2021-09-28 11:34:27 +0200

    je tente le egal pas simple

---
+++ calculatrice.js
@@ -0,0 +1,7 @@
+function appuitouche(button) {
+    document.getElementById("ecran").innerHTML+=button
+    }
+//function appuitouchegal(button) {
+//       ifdocument.getElementById("ecran").innerHTML 
+//       else 
+//    }
