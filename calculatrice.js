671356206ff57df797604862edd29f8a16acde7e
Bryan <squeld@outlook.fr>
2021-09-28 13:41:05 +0200

    Final avant corection

--- calculatrice.js
+++ calculatrice.js
@@ -4,5 +4,11 @@ function appuitouche(button) {
 //function resultats(button) {
 //       ifdocument.getElementById("ecran").innerHTML 
 //       else 
-//    }
- 
+//   
+
+let ecran = document.getElementById("ecran").innerHTML
+ecran.split('+')
+ecran.indexOf(button++)
+function resultats(button) {
+    document.indexOf 
+}
